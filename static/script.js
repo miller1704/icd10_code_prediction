2b16d672c1da14d2863ab2a20aadef3569a32fa5
Sean Miller <seanmiller@Seans-MacBook-Pro.local>
2022-06-03 14:25:17 -0700

    second commit

--- static/script.js
+++ static/script.js
@@ -11,25 +11,39 @@ var quill = new Quill('#editor', {
             });
 
 
+//initiate seconds counter to prevent over-querying
+function currSecond() {
+  return parseInt(Date().slice(22,24));
+};
 
+//define globals
+let currentRequest = null;
+let priorTime = currSecond();
+let currTime = currSecond();
 
-currentRequest = quill.on('text-change', function() {
-  let currentRequest = null;
+//define listener as
+quill.on('text-change', function() {
+  //setup real-time variables
   let input_length = quill.getLength()-1
-  if(input_length > 30) {
+  currTime = currSecond();
+  //run prediction where 5 characters exist, 5 seconds has passed from prior run, and prior prediction has not been made
+  if((input_length > 5) && (Math.abs(currTime - priorTime) >= 5) && (currentRequest == null)) {
+    console.log('text valid for prediction run!')
     let text_start = Math.max(input_length - 200,0)
-    let text = quill.getContents()['ops'][0]['insert'].slice(text_start, input_length)
-    let last_char = text[text.length-1]
-    if (last_char == ' ') {         
-       currentRequest = $.ajax({
-          dataType: "json"
-          ,url: '/prediction'
-          ,beforeSend : function(xhr, opts) {if(currentRequest != null) {console.log('toomuch!'); xhr.abort();}}
-          ,data: {text: text}
-          ,success : function(data) {$("#icd10_code").text(data.icd10_code);$("#icd10_description").text('Official Description: \n'+data.icd10_description);}
-        });
-     }
-
+    let text = quill.getContents()['ops'][0]['insert'].slice(text_start, input_length)      
+    currentRequest = $.ajax({
+      dataType: "json"
+      ,url: '/prediction'
+      ,beforeSend : function(xhr, opts) {            
+        priorTime = currTime;
+      }
+      ,data: {text: text}
+      ,success : function(data) {
+        $("#icd10_code").text(data.icd10_code);
+        $("#icd10_description").text('Official Description: \n'+data.icd10_description);
+        currentRequest = null;
+      }
+    });
   }
 });
 
